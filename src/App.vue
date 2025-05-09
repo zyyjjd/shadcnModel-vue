a18b2f809eb0d9d86df07b556ffbaab7cd5bc233
jinjd <2499124163@qq.com>
2025-05-09 10:33:10 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import PerspectiveCard from '@/components/card/PerspectiveCard.vue';
+import ParticlesBg from '@/components/ui/particles-bg/ParticlesBg.vue';
+import RippleButton from '@/components/ui/ripple-button/RippleButton.vue';
+import IInput from '@/components/ui/input/IInput.vue';
+import { computed } from "vue";
+import { useColorMode } from "@vueuse/core";
+
+const isDark = computed(() => useColorMode().value == "dark");
+
+const change = (e: any) => {
+  console.log(e.target.value,'????');
+}
+</script>
+
+<template>
+ <div
+    class="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl"
+  >
+    <span
+      class="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10"
+    >
+      Hello World
+    </span>
+    <IInput id="input-demo" placeholder="请输入内容" @change="change" containerClass="inputClass"/>
+    <PerspectiveCard class="z-10"/>
+    <RippleButton class="z-20">
+      <span>Click Me</span>
+    </RippleButton>
+    <ParticlesBg
+      class="absolute inset-0"
+      :quantity="100"
+      :ease="100"
+      :color="isDark ? '#FFF' : '#000'"
+      :staticity="10"
+      refresh
+    />
+  </div>
+</template>
+
+<style scoped>
+.inputClass {
+  z-index: 10;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+import AutoImport from "unplugin-auto-import/vite";
+ 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+      ],
+      eslintrc: {
+        enabled: true,
+      },
+      imports: ["vue", "vue-router"],
+    }),
+  ],
+  resolve: {
+    extensions: ['.vue', '.ts'],
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  },
+})
