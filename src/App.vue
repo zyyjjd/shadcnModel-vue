fd2b58c3384411d8e73eae15bad475f053675644
jinjd <2499124163@qq.com>
2025-05-13 17:41:45 +0800

    feat: 添加GSAP动画和简单透视卡片组件
    在App.vue中引入GSAP库，并实现滚动触发的动画效果。新增simplePerspectiveCard.vue组件，用于展示带有3D透视效果的卡片。同时更新了package.json和pnpm-lock.yaml以添加GSAP依赖。

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,157 @@
 <script setup lang="ts">
 import PerspectiveCard from '@/components/card/PerspectiveCard.vue';
+import SimplePerspectiveCard from '@/components/card/simplePerspectiveCard.vue';
 import ParticlesBg from '@/components/ui/particles-bg/ParticlesBg.vue';
 import RippleButton from '@/components/ui/ripple-button/RippleButton.vue';
 import IInput from '@/components/ui/input/IInput.vue';
-import { computed } from "vue";
+import { computed, onMounted, onUnmounted, ref } from "vue";
 import { useColorMode } from "@vueuse/core";
+import gsap from 'gsap';
+import ScrollTrigger from "gsap/ScrollTrigger.js";
+import Bg from '@/assets/bg_1.jpg';
 
 const isDark = computed(() => useColorMode().value == "dark");
 
+gsap.registerPlugin(ScrollTrigger);
+
+let t1: gsap.core.Timeline | null = null;
+let t2: gsap.core.Timeline | null = null;
+
 const change = (e: any) => {
-  console.log(e.target.value,'????');
+  console.log(e.target.value, '????');
 }
+
+onMounted(() => {
+  t1 = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#scroll-trigger-wrapper",
+        start: "top top",
+        end: "+=800",
+        scrub: 0.5,
+        pin: true, // 当滚动条滚动时，保持元素固定在视窗内
+        markers: true,
+      },
+    });
+
+    t1
+    .to("#scroll-trigger", {
+      scale: 1.2,
+    })
+    .to('#Introduction',{
+      opacity:0,
+    },'<=')
+
+
+  t2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: '#card-container',
+      start: '-=400',
+      end: 'top',
+      scrub: true,
+      pin: false,
+      // markers: true
+    }
+  });
+
+  t2
+      // .fromTo('#project-1 h3, #project-1 p', { color: 'transparent' }, { color: '#6d28d9' })
+      // .fromTo('#project-2 h3, #project-2 p', { color: 'transparent' }, { color: '#6d28d9' }, '<=')
+      // .fromTo('#project-3 h3, #project-3 p', { color: 'transparent' }, { color: '#6d28d9' }, '<=')
+      // .fromTo('#project-4 h3, #project-4 p', { color: 'transparent' }, { color: '#6d28d9' }, '<=')
+      .fromTo('#project-1', { xPercent: 50, yPercent: 50,duration:1}, {xPercent: 0,yPercent:0})
+      .fromTo('#project-2', { xPercent: -50, yPercent: 50,duration:1}, {xPercent: 0,yPercent:0},'<=')
+      .fromTo('#project-3', { xPercent: 50, yPercent: -50,duration:1}, {xPercent: 0,yPercent:0},'<=')
+      .fromTo('#project-4', { xPercent: -50, yPercent: -50,duration:1}, {xPercent: 0,yPercent:0},'<=')
+      .to('#project-1 h3, #project-1 p', { color: '#6d28d9' },'+=0.3')
+      .to('#project-2 h3, #project-2 p', { color: '#6d28d9' }, '<=')
+      .to('#project-3 h3, #project-3 p', { color: '#6d28d9' }, '<=')
+      .to('#project-4 h3, #project-4 p', { color: '#6d28d9' }, '<=');
+
+
+})
+
+onUnmounted(() => {
+  t1?.kill();
+  t2?.kill();
+})
+
 </script>
 
 <template>
-   <ParticlesBg
-      class="fixed w-full h-screen inset-0 z-[-1]"
-      :quantity="100"
-      :ease="100"
-      :color="isDark ? '#FFF' : '#000'"
-      :staticity="10"
-      refresh
-    />
- <div
-    class="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-transparent"
-  >
-    <span
-      class="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10"
-    >
-      Hello World
-    </span>
-    <IInput id="input-demo" placeholder="请输入内容" @change="change" containerClass="inputClass"/>
-    <PerspectiveCard class="z-10"/>
-    <RippleButton class="z-20">
-      <span>Click Me</span>
-    </RippleButton>
-  </div>
-  <div
-    class="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-transparent"
-  >
-    <span
-      class="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10"
-    >
-      Hello World
-    </span>
-    <IInput id="input-demo" placeholder="请输入内容" @change="change" containerClass="inputClass"/>
-    <PerspectiveCard class="z-10"/>
-    <RippleButton class="z-20">
-      <span>Click Me</span>
-    </RippleButton>
+  <div class="overflow-x-hidden">
+      <!-- 简介 -->
+  <section class="w-full h-screen overflow-hidden flex items-center justify-center" id="scroll-trigger-wrapper">
+    <img class="w-full h-full object-cover absolute inset-0" :src="Bg" alt="" id='scroll-trigger'/>
+    <div
+      className="flex flex-col justify-center items-center px-4 z-10"
+      id="Introduction">
+      <div className="max-w-3xl mx-auto text-center">
+        <!-- <h1 class="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+          Hello World
+        </h1> -->
+        <h1 className="text-4xl md:text-6xl font-bold mb-6 text-purple-900">hello world</h1>
+        <p className="text-xl md:text-2xl mb-8 text-pink-300">
+          I focus on creating beautiful and powerful websites and
+          applications, and I love exploring new technologies and creative solutions.
+        </p>
+      </div>
+    </div>
+  </section>
+  <!-- 卡片 -->
+  <section class="flex items-center px-4 py-24 z-40 justify-center relative" id='card-container'>
+    <div class="bg-red-500 w-[100px] h-[100px] relative"></div>
+    <!-- <SimplePerspectiveCard>
+        <template #title>
+          <div>我的项目</div>
+        </template>
+        <template #desc>
+          <span>以下是我最近完成的一些项目，展示了我在前端和后端开发方面的技能和经验。
+            每个项目都代表了我对用户体验和技术实现的追求。</span>
+        </template>
+        <template #content>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="rounded-lg bg-purple-100 p-6 border-0 project-card" id='project-1'>
+              <h3 className="text-xl font-semibold mb-2 text-transparent">电子商务平台</h3>
+              <p className="text-transparent">
+                使用 Next.js 和 Tailwind CSS 构建的现代电子商务网站，包含产品展示、购物车和支付功能。
+              </p>
+            </div>
+
+            <div className="rounded-lg bg-purple-100 p-6 border-0 project-card" id='project-2'>
+              <h3 className="text-xl font-semibold mb-2 text-transparent">社交媒体应用</h3>
+              <p className="text-transparent">
+                使用 Next.js 和 Tailwind CSS 构建的现代电子商务网站，包含产品展示、购物车和支付功能。
+              </p>
+            </div>
+
+            <div className="rounded-lg bg-purple-100 p-6 border-0 project-card" id='project-3'>
+              <h3 className="text-xl font-semibold mb-2 text-transparent">数据可视化仪表板</h3>
+              <p className="text-transparent">
+                使用 D3.js 和 Vue.js 构建的交互式数据可视化仪表板，帮助用户理解复杂数据。
+              </p>
+            </div>
+
+            <div className="rounded-lg bg-purple-100 p-6 border-0 project-card" id='project-4'>
+              <h3 className="text-xl font-semibold mb-2 text-transparent">移动应用程序</h3>
+              <p className="text-transparent">
+                使用 React Native 开发的跨平台移动应用，提供流畅的用户体验和离线功能。
+              </p>
+            </div>
+          </div>
+        </template>
+      </SimplePerspectiveCard> -->
+  </section>
+
+
+  <!-- 粒子特效展示 -->
+  <!-- <section class="w-full h-screen relative">
+    <ParticlesBg class="absolute inset-0" :quantity="100" :ease="100" :color="isDark ? '#FFF' : '#000'" :staticity="10"
+      refresh>
+    </ParticlesBg>
+  </section> -->
+  <section class="w-full h-screen bg-green-400">
+
+  </section>
   </div>
 </template>
 
---
+++ src/components/card/simplePerspectiveCard.vue
@@ -0,0 +1,70 @@
+<template>
+    <ClientOnly>
+      <CardContainer>
+        <CardBody
+          class="group/card relative size-auto rounded-xl border border-black/[0.1] bg-gray-50 p-6 sm:w-[30rem] dark:border-white/[0.2] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1]"
+        >
+        <!-- 标题 -->
+          <CardItem
+            :translate-z="50"
+            class="text-xl font-bold text-neutral-600 dark:text-white"
+          >
+           <slot name="title"/>
+          </CardItem>
+          <!-- 描述 -->
+          <CardItem
+            v-if="$slots.desc"
+            as="p"
+            translate-z="60"
+            class="mt-2 max-w-sm text-sm text-neutral-500 dark:text-neutral-300"
+          >
+           <slot name="desc"/>
+          </CardItem>
+          <!-- 内容 -->
+          <CardItem
+            :translate-z="100"
+            class="mt-4 w-full"
+          >
+            <!-- <img
+              src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              height="1000"
+              width="1000"
+              class="h-60 w-full rounded-xl object-cover group-hover/card:shadow-xl"
+              alt="thumbnail"
+            /> -->
+            <slot name="content"/>
+          </CardItem>
+          <div class="mt-20 flex items-center justify-end">
+            <!-- <CardItem
+              :translate-z="20"
+              as="a"
+              href="https://rahulv.dev"
+              target="__blank"
+              class="rounded-xl px-4 py-2 text-xs font-normal dark:text-white"
+            >
+              <slot name="link"/>
+            </CardItem> -->
+            <CardItem
+                v-if="$slots.button"
+                :translate-z="20"
+                as="button"
+            >
+                <slot name="button"/>
+                <!-- <InteractiveHoverButton @click="onClick" :text="'开始'"/> -->
+            </CardItem>
+           
+          </div>
+        </CardBody>
+      </CardContainer>
+    </ClientOnly>
+  </template>
+  
+  <script setup lang="ts">
+  import { CardContainer, CardBody, CardItem } from "@/components/ui/card-3d";
+//   import InteractiveHoverButton from "@/components/ui/interactive-hover-button/InteractiveHoverButton.vue";
+
+//   function onClick() {
+//    console.log('click');
+   
+//   }
+  </script>
